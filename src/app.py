581f10e015d2eb243f750ff4c632498a7e7cbfcd
Mohammad Omer Aamir <muhammad01aamir@gmail.com>
2025-03-05 08:25:23 +0500

    testing automation

--- src/app.py
+++ src/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello, Jenkins!"
+    return "Hello, Jenkins using automation!"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
