0f716a2675c9685c7f1260b7f7ce1d3c50efdbd4
Mohammad Omer Aamir <126745630+momer99@users.noreply.github.com>
2025-03-07 08:24:51 +0500

    Update model.py

--- src/model.py
+++ src/model.py
@@ -26,7 +26,10 @@ df['review'] = df['review'].apply(clean_text)
 
 # Split data
 X_train, X_test, y_train, y_test = train_test_split(
-    df['review'], df['sentiment'], test_size=0.2, random_state=42
+    df['review'],
+    df['sentiment'],
+    test_size=0.2,
+    random_state=42,
 )
 
 
